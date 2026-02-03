cf16159d7c8e20374d851b5765250e5d9b2bd861
mikii.pedia <pediamikii@gmail.com>
2026-02-03 18:10:31 +0700

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+// main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./src/App";  // <-- TAMBAHKAN /src/
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
